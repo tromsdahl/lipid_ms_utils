519f6286aab1afaeaa68da678281496558846e75
trom2 <romsdahl323@gmail.com>
2025-08-18 04:29:08 -0500

    20250818: test code

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::io::{self, Read};
 
 fn main() {
 
-    println!("Lipid MS Utilities\nSelect an option:");
-    println!("1. Lipid mass calculator");
-    println!("Press 'q' to quit.");
+    println!("Lipid MS Utilities");
+    println!("Enter a number to select an option:");
 
-    loop {
-        println!("Enter a number to select an option:");
+    loop {        
+        
+        println!("1. Lipid mass calculator");
+        println!("Press 'q' to quit.");
 
         let mut user_selection = String::new();
 
@@ -19,6 +20,8 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        println!("You entered: {user_selection}");
 
     match user_selection {
         1 => lipid_calculator,
@@ -44,6 +47,24 @@ mod lipid_calculator {
     const SODIUM: f64 = 22.989769;
     const POTASSIUM: f64 = 38.963707;
     const SULFUR: f64 = 31.972072;
+
+    fn lipid_calc_funct () {
+        loop (
+            println!("Enter in the lipid class:");
+            println!("Options:");
+            println!("TG");
+
+            let mut lipid_class = String::new();
+
+            io::stdin()
+                .read_line(&mut lipid_class)
+                .expect("Failed to read line.");
+
+            
+
+
+        )
+    }
 
     fn tag_mw(fa_c: f64, fa_des: f64) -> String {
         let monoiso_mass = (CARBON * (fa_c + 3.0)) + (HYDROGEN * ((fa_c * 2.0) - (fa_des * 2.0) + 2.0)) + (OXYGEN * 6.0);
